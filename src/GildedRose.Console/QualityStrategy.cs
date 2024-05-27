65ee316da8e65ab123f8c0c78448be98696c067b
tom-van-acker <tom_van_acker@yahoo.com>
2024-05-27 19:59:53 +0200

    Implementing different QualityStrategies - unittests pass

--- src/GildedRose.Console/QualityStrategy.cs
+++ src/GildedRose.Console/QualityStrategy.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.SqlServer.Server;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,9 +17,31 @@ namespace GildedRose.Console
 
     public class DefaultQualityStrategy : IQualityStrategy
     {
+        public static void CheckBounds(Item item,int lowest, int highest)
+        {
+            if (item.Quality < lowest)
+            {
+                item.Quality = lowest;
+            }
+            if (item.Quality > highest)
+            {
+                item.Quality = highest;
+            }
+        }
+
+        public static int CalcDecrease(int sellIn)
+        {
+            if (sellIn <= 0)
+            {
+                return 2;
+            }
+            return 1;
+        }
+
         public void UpdateQuality(Item item)
         {
-            throw new NotImplementedException();
+            item.Quality -= CalcDecrease(item.SellIn);
+            CheckBounds(item, 0, 50);
         }
     }
 
@@ -26,31 +49,67 @@ namespace GildedRose.Console
     {
         public void UpdateQuality(Item item)
         {
-            throw new NotImplementedException();
+            item.Quality += 1;
+            DefaultQualityStrategy.CheckBounds(item, 0, 50);
         }
     }
 
     public class SulfurasQualityStrategy : IQualityStrategy
     {
+
+        public SulfurasQualityStrategy()
+        {
+        }
         public void UpdateQuality(Item item)
         {
-            throw new NotImplementedException();
+            // Sulfras never decreases in quality, and should never be sold
+            item.Quality = 80;
         }
     }
 
     public class BackstagePassQualityStrategy : IQualityStrategy
     {
+        /// <summary>
+        /// This strategy causes an increase in Quality as it's SellIn value approaches; 
+        /// Quality increases by 2 when there are 10 days or less and by 3 when there 
+        /// are 5 days or less but Quality drops to 0 after the concert
+        /// </summary>
+        /// <param name="item"></param>
+        /// <exception cref="NotImplementedException"></exception>
         public void UpdateQuality(Item item)
         {
-            throw new NotImplementedException();
+            if (item.SellIn <= 0)
+            {
+                item.Quality = 0;
+            }
+            if (item.SellIn >0 && item.SellIn <= 5)
+            {
+                item.Quality += 3;
+            }
+            if (item.SellIn > 5 && item.SellIn <= 10)
+            {
+                item.Quality += 2;
+            }
+            if (item.SellIn > 10)
+                item.Quality++;
+
+            DefaultQualityStrategy.CheckBounds(item, 0, 50);
+
+
         }
     }
 
     public class ConjuredQualityStrategy : IQualityStrategy
     {
+        /// <summary>
+        /// "Conjured" items degrade in Quality twice as fast as normal items
+        /// </summary>
+        /// <param name="item"></param>
+        /// <exception cref="NotImplementedException"></exception>
         public void UpdateQuality(Item item)
         {
-            throw new NotImplementedException();
+            item.Quality -= 2*DefaultQualityStrategy.CalcDecrease(item.SellIn);
+            DefaultQualityStrategy.CheckBounds(item, 0, 50);
         }
     }
 
--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -45,16 +45,23 @@ namespace GildedRose.Tests
         [Fact]
         public void TestDefaultQualityStrategy()
         { 
-            // The default strategy is to decrease quality by 1
+            // The default strategy is to decrease quality everytime by 1
             var item = new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 };
             var strategy = new DefaultQualityStrategy();
             strategy.UpdateQuality(item);
             Assert.Equal(19, item.Quality);
+            strategy.UpdateQuality(item);
+            Assert.Equal(18, item.Quality);
 
             // Quality should never be negative
             item = new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 0 };
             strategy.UpdateQuality(item);
             Assert.Equal(0, item.Quality);
+
+            // Quality should never increase above 50
+            item = new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 60 };
+            strategy.UpdateQuality(item);
+            Assert.Equal(50, item.Quality);
 
             // Quality should decrease by 2 after sell by date
             item = new Item { Name = "+5 Dexterity Vest", SellIn = 0, Quality = 20 };
@@ -66,8 +73,101 @@ namespace GildedRose.Tests
             item = new Item { Name = "+5 Dexterity Vest", SellIn = 0, Quality = 1 };
             strategy.UpdateQuality(item);
             Assert.Equal(0, item.Quality);
+        }
 
+        [Fact]
+        public void TestAgedBrieQualityStrategy()
+        {
+            // Aged Brie increases in quality as it gets older
+            var item = new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 };
+            var strategy = new AgedBrieQualityStrategy();
+            strategy.UpdateQuality(item);
+            Assert.Equal(1, item.Quality);
+            strategy.UpdateQuality(item);
+            Assert.Equal(2, item.Quality);
+
+            // Quality should never increase above 50
+            item = new Item { Name = "Aged Brie", SellIn = 2, Quality = 50 };
+            strategy.UpdateQuality(item);
+            Assert.Equal(50, item.Quality);
 
         }
+
+        [Fact]
+        public void TestSulfurasQualityStrategy()
+        {
+            // Sulfuras never decreases in quality
+            var item = new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 0 };
+            var strategy = new SulfurasQualityStrategy();
+            strategy.UpdateQuality(item);
+            Assert.Equal(80, item.Quality);
+        }
+
+        [Fact]  
+        public void TestBackstagePassQualityStrategy()
+        {
+            // Backstage passes increase in quality as the concert approaches
+            var item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 };
+            var strategy = new BackstagePassQualityStrategy();
+            strategy.UpdateQuality(item);
+            Assert.Equal(21, item.Quality);
+
+            // Quality should increase by 2 when there are 10 days or less
+            item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 20 };
+            strategy.UpdateQuality(item);
+            Assert.Equal(22, item.Quality);
+            strategy.UpdateQuality(item);
+            Assert.Equal(24, item.Quality);
+
+            // Quality should increase by 3 when there are 5 days or less
+            item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 20 };
+            strategy.UpdateQuality(item);
+            Assert.Equal(23, item.Quality);
+            strategy.UpdateQuality(item);
+            Assert.Equal(26, item.Quality);
+
+            // Quality should never increase above 50
+            item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 4, Quality = 48 };
+            strategy.UpdateQuality(item);
+            Assert.Equal(50, item.Quality);
+            strategy.UpdateQuality(item);
+            Assert.Equal(50, item.Quality);
+
+            // Quality should drop to 0 after the concert
+            item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 20 };
+            strategy.UpdateQuality(item);
+            Assert.Equal(0, item.Quality);
+        }
+
+        [Fact]
+        public void TestConjuredQualityStrategy()
+        {
+            // Conjured items degrade in Quality twice as fast as normal items
+            var item = new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 };
+            var strategy = new ConjuredQualityStrategy();
+            strategy.UpdateQuality(item);
+            Assert.Equal(4, item.Quality);
+            strategy.UpdateQuality(item);
+            Assert.Equal(2, item.Quality);
+
+            // Quality should never be negative
+            item = new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 1 };
+            strategy.UpdateQuality(item);
+            Assert.Equal(0, item.Quality);
+
+            // Quality should decrease by 4 after sell by date
+            item = new Item { Name = "Conjured Mana Cake", SellIn = 0, Quality = 20 };
+            strategy.UpdateQuality(item);
+            Assert.Equal(16, item.Quality);
+            strategy.UpdateQuality(item);
+            Assert.Equal(12, item.Quality);
+
+            // But never be negative
+            strategy = new ConjuredQualityStrategy();
+            item = new Item { Name = "Conjured Mana Cake", SellIn = 0, Quality = 1 };
+            strategy.UpdateQuality(item);
+            Assert.Equal(0, item.Quality);
+        }
+
      }
 }
